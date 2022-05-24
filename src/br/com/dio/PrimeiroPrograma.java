1dfafe75fc996abfa0df0c68a6c527c8cc0e247c
Aline013 <alineoliveiracp@gmail.com>
2022-05-24 07:12:08 -0300

    Criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,15 +1,16 @@
 package br.com.dio;
 
+
 import br.com.dio.model.Gato;
 
-import javax.sql.rowset.serial.SQLOutputImpl;
-
 public class PrimeiroPrograma {
+
     public static void main(String[] args) {
-
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O problema dos 3 corpos",300);
+        System.out.println(livro1);
 
         /*int a = 5;
         int b = 3;
@@ -17,5 +18,39 @@ public class PrimeiroPrograma {
         System.out.println("Hello, World! " + (a+b));*/
     }
 
+}
+
+
+class Livro {
+    private String nome;
+    private Integer numPgs;
+
+    public Livro(String nome, Integer numPgs) {
+        this.nome = nome;
+        this.numPgs = numPgs;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPgs() {
+        return numPgs;
+    }
+
+    public void setNumPgs(Integer numPgs) {
+        this.numPgs = numPgs;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPgs='" + numPgs + '\'' +
+                '}';
+    }
+}
